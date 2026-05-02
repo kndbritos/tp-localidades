aac13db88ebf1f6d76b8519d9044ffa558c67fd6
MatiParra <mlongoparra@gmail.com>
2026-05-02 18:50:34 -0300

    Estructura clase localidad

--- src/modelo/Localidad.java
+++ src/modelo/Localidad.java
@@ -1,5 +1,24 @@
 package modelo;
 
 public class Localidad {
-
+	private String nombre;
+	private String provincia;
+	private double latitud;
+	private double longitud;
+	private int idVertice;
+	
+	public Localidad(String nombre, String provincia, double latitud, double longitud, int idVertice) {
+        this.nombre = nombre;
+        this.provincia = provincia;
+        this.latitud = latitud;
+        this.longitud = longitud;
+        this.idVertice = idVertice;
+    }
+	
+	
+	public String getNombre() { return nombre; }
+	public String getProvincia() { return provincia; }
+	public double getLatitud() { return latitud; }
+	public double getLongitud() { return longitud; }
+	public int getIdVertice() { return idVertice; }
 }
