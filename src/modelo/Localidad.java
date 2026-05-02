1840ff89cc710c59c1a03cf5a3367085c42937c0
MatiParra <mlongoparra@gmail.com>
2026-05-02 19:29:21 -0300

    clase LocalidadTest

--- src/modelo/Localidad.java
+++ src/modelo/Localidad.java
@@ -8,6 +8,21 @@ public class Localidad {
 	private int idVertice;
 	
 	public Localidad(String nombre, String provincia, double latitud, double longitud, int idVertice) {
+		
+		if (nombre == null || nombre.length() == 0)
+			throw new IllegalArgumentException("El nombre de la localidad no puede estar vacío");
+		
+		if (provincia == null || provincia.trim().isEmpty())
+			throw new IllegalArgumentException("La provincia no puede estar vacía");
+		
+		if (latitud < -90.0 || latitud > 90.0)
+			throw new IllegalArgumentException("Latitud inválida. Debe estar entre -90 y 90");
+		
+		if (longitud < -180.0 || longitud > 180.0)
+			throw new IllegalArgumentException("Longitud inválida. Debe estar entre -180 y 180");
+		
+		if (idVertice < 0)
+			throw new IllegalArgumentException("El ID del vértice no puede ser negativo");
         this.nombre = nombre;
         this.provincia = provincia;
         this.latitud = latitud;
--- src/test/LocalidadTest.java
+++ src/test/LocalidadTest.java
@@ -1,5 +1,40 @@
 package test;
 
-public class LocalidadTest {
+import modelo.Localidad;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
+public class LocalidadTest {
+	
+	
+	@Test
+	public void testLocalidadSeCreaCorrectamente() {
+		Localidad localidad = new Localidad("San Miguel", "Buenos Aires", -34.54, -58.71, 0);
+		
+		assertEquals("San Miguel", localidad.getNombre());
+		assertEquals("Buenos Aires", localidad.getProvincia());
+		
+		assertEquals(-34.54, localidad.getLatitud(), 0.001);
+		assertEquals(-58.71, localidad.getLongitud(), 0.001);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testLocalidadConNombreVacio() {
+		Localidad localidad = new Localidad("", "Buenos Aires", -34.54, -58.71, 0);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testLocalidadConProvinciaVacia() {
+		Localidad localidad = new Localidad("San Miguel", "", -34.54, -58.71, 0);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testLocalidadConLatitudIncorrecta() {
+		Localidad localidad = new Localidad("San Miguel", "Buenos Aires", -95.15, -58.71, 0);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testLocalidadConLongitudIncorrecta() {
+		Localidad localidad = new Localidad("San Miguel", "Buenos Aires", -34.54, -200.00, 0);
+	}
 }
