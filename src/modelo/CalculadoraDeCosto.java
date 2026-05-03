39ef159c529c84321aa956229a945aeb2ddda343
MatiParra <mlongoparra@gmail.com>
2026-05-03 13:33:49 -0300

    clases AsignacionDeLocalidades y CalculadoraDeCosto

---
+++ src/modelo/CalculadoraDeCosto.java
@@ -0,0 +1,34 @@
+package modelo;
+
+public class CalculadoraDeCosto {
+
+	// Formula de Haversine para calcular distancia en km entre 2 coordenadas reales
+	private double calcularDistanciaEnKm(Localidad loc1, Localidad loc2) {
+        final int RADIO_TIERRA = 6371;
+        double latDist = Math.toRadians(loc2.getLatitud() - loc1.getLatitud());
+        double lonDist = Math.toRadians(loc2.getLongitud() - loc1.getLongitud());
+        double a = Math.sin(latDist / 2) * Math.sin(latDist / 2)
+                 + Math.cos(Math.toRadians(loc1.getLatitud())) * Math.cos(Math.toRadians(loc2.getLatitud()))
+                 * Math.sin(lonDist / 2) * Math.sin(lonDist / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return RADIO_TIERRA * c; 
+    }
+	
+	
+	public double calcularCostoConexion(Localidad loc1, Localidad loc2, double costoPorKm) {
+        
+        double distancia = calcularDistanciaEnKm(loc1, loc2);
+        
+        double costoFinal = distancia * costoPorKm;
+
+        if (distancia > 300) {
+            costoFinal = costoFinal + (costoFinal * 0.40);
+        }
+
+        if (!loc1.getProvincia().equals(loc2.getProvincia())) {
+            costoFinal = costoFinal + 20000;
+        }
+
+        return costoFinal;
+    }
+}
