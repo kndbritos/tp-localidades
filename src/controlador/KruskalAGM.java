0b0400b6d71c8736babc80fdb1cb64f97c1b7519
kndbritos <candelabritos2004@gmaiil.com>
2026-05-03 03:28:38 -0300

    clase Servicio y Resultado Planificacion

--- src/controlador/KruskalAGM.java
+++ src/controlador/KruskalAGM.java
@@ -1,5 +1,15 @@
 package controlador;
 
+import java.util.Set;
+
+import modelo.AristaConPeso;
+import modelo.GrafoConPeso;
+
 public class KruskalAGM {
+
+	public Set<AristaConPeso> obtenerAGM(GrafoConPeso grafo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
